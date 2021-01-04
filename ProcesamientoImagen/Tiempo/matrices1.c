161402be54b7a3ca9ddc24712e02e788dd0e72ad
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2021-01-04 00:01:12 -0600

    Comparando velocidad de ejecución al recorrer una matriz

---
+++ ProcesamientoImagen/Tiempo/matrices1.c
@@ -0,0 +1,84 @@
+/*
+Este programa muestra la diferencia de tiempo al recorrer una matriz
+por filas y por columnas.
+La función sumaMatrices recorre por fila y la función sumaMatrices2 
+por columna
+*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#define WIDTH  8000
+#define HEIGHT 7000
+
+int * reservarMemoria( void );
+void sumaMatrices( int *A, int *B, int *C );
+void sumaMatrices2( int *A, int *B, int *C );
+void llenarArreglo( int *matriz );
+
+int main( void ){
+	int *A, *B, *C;
+
+	A = reservarMemoria();
+	B = reservarMemoria();
+	C = reservarMemoria();
+
+	llenarArreglo( A );
+	llenarArreglo( B );
+	llenarArreglo( C );
+
+	sumaMatrices( A, B, C );
+	//sumaMatrices2( A, B, C );
+
+	free( A );
+	free( B );
+	free( C );
+
+	return 0;
+}
+
+void llenarArreglo( int *matriz ){
+	register int x, y;
+	int indice;
+
+	for( y = 0 ; y < HEIGHT ; y++ )
+		for( x = 0 ; x < WIDTH ; x++ ){
+			indice = (y * WIDTH + x);
+			matriz[indice] = rand() % 8192;
+		}
+}
+
+
+void sumaMatrices( int *A, int *B, int *C ){
+	register int x, y;
+	int indice;
+	
+	for( y = 0 ; y < HEIGHT ; y++ )
+		for( x = 0 ; x < WIDTH ; x++ ){
+			indice = y * WIDTH + x;
+			C[indice] = A[indice] + B[indice];
+		}
+}
+
+void sumaMatrices2( int *A, int *B, int *C ){
+	register int x, y;
+	int indice;
+	
+	for( x = 0 ; x < WIDTH ; x++ )
+		for( y = 0 ; y < HEIGHT ; y++ ){
+			indice = y * WIDTH + x;
+			C[indice] = A[indice] + B[indice];
+		}
+}
+
+int * reservarMemoria( void ){
+	int *mem;
+
+	mem = (int *)malloc( sizeof(int) * WIDTH * HEIGHT );
+	if( !mem ){
+		perror("Error de asignación de memoria");
+		exit(EXIT_FAILURE);
+	}
+
+	return mem;
+}
+
